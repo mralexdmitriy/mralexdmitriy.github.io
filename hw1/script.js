e6af04cfe29bdba9fc39287980324bd9d12c1551
Dmitriy <mr.alexdmitriy@mail.ru>
2020-01-09 19:43:42 +0300

    fix

---
+++ hw1/script.js
@@ -0,0 +1,16 @@
+// var query_array = ["query1", "query2", "quesry3"]
+
+
+
+var query_array = ["\"All successful men and women are big dreamers. They imagine what their future could be, ideal in every respect, and then \
+they work every day toward their distant vision, that goal or purpose.\" – Brian Tracy",
+"\"I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best.\" ― Marilyn Monroe",
+"\"Be yourself; everyone else is already taken.\" ― Oscar Wilde",
+
+ "\"Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.\" ― Albert Einstein"]
+
+function myFunction(x){
+  console.log(x)
+}
+query_array.forEach(myFunction);
+console.log("We are the so-called \"Vikings\" from the north.")
