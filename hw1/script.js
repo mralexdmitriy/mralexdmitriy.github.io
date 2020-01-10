337cb47de0f415f1d79f68ef63c2754e14d3e0f1
mr.alexdmitriy <mr.alexdmitriy@mail.ru>
2020-01-10 03:59:52 +0300

    new commit

--- hw1/script.js
+++ hw1/script.js
@@ -1,16 +1,18 @@
 // var query_array = ["query1", "query2", "quesry3"]
 
-
-
 var query_array = ["\"All successful men and women are big dreamers. They imagine what their future could be, ideal in every respect, and then \
 they work every day toward their distant vision, that goal or purpose.\" – Brian Tracy",
 "\"I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best.\" ― Marilyn Monroe",
-"\"Be yourself; everyone else is already taken.\" ― Oscar Wilde",
+"\"Twenty years from now you will be more disappointed by the things that you didn't do than by the ones you did do. So throw off the bowlines. Sail away from the safe harbor. Catch the trade winds in your sails. Explore. Dream. Discover.\" ― H. Jackson Brown Jr.",
+ "\"The opposite of love is not hate, it's indifference. The opposite of art is not ugliness, it's indifference. The opposite of faith is not heresy, it's indifference. And the opposite of life is not death, it's indifference.\" ― Elie Wiesel"]
+var index = 0
+var query_cont = query_array.length
 
- "\"Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.\" ― Albert Einstein"]
 
-function myFunction(x){
-  console.log(x)
+function myFunction() {
+  index = index + 1
+  if (index > query_cont-1) {
+    index = 0
+  }
+  document.getElementById("inner").innerHTML = query_array[index];
 }
-query_array.forEach(myFunction);
-console.log("We are the so-called \"Vikings\" from the north.")
